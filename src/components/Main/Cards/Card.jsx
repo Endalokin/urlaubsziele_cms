6755c896aa22234e5b5f9c6bdaa7ab5700e5479f
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-11-01 19:56:58 +0100

    static router- cards link to detailPage with static param

--- src/components/Main/Cards/Card.jsx
+++ src/components/Main/Cards/Card.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import fetchData from '../../../utils/fetchAPI';
+import { NavLink } from 'react-router-dom';
 
 export default function Card({country, imgId, text}) {
     const url = `https://cdn.contentful.com/spaces/8es1vct37z1y/assets/${imgId}?access_token=z9I4_II-o6y3ZX7lTgQxu2EU3ctK6C0dvKqGqG7JXKs`;
@@ -17,7 +18,7 @@ export default function Card({country, imgId, text}) {
         <>
             <div className="col">
                 <div className="td_selector card h-100 shadow border-light active" data-traveldest="Frankreich" style={{ overflow: "hidden" }}>
-                    <a href="#div_id" className="link-dark link-underline link-underline-opacity-0">
+                    <NavLink to="detailPage/11kuRvuGnGSd85UbY0i5ao" className="link-dark link-underline link-underline-opacity-0">
                         <img src={imgUrl} alt="" className="card-img-top object-fit-cover" style={{ height: "30vh", minHeight: "4em" }} />
                         <div className="card-img-overlay d-md-none text-center text-bg-dark bg-opacity-50" style={{ height: "10vh", minHeight: "2em", margin: "auto", display: "flex", justifyContent: "center", alignItems: "center" }}>
                             <h2><strong>{country}</strong></h2>
@@ -28,7 +29,7 @@ export default function Card({country, imgId, text}) {
                                 {text}
                             </p>
                         </div>
-                    </a>
+                    </NavLink>
                 </div>
             </div>
         </>
---
+++ src/components/Main/RootLayout.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import { Outlet } from "react-router-dom";
+
+export default function RootLayout()
+{
+
+    return(
+        <div className="root-layout">
+            <Header/>
+        <main>
+        <Outlet />
+        </main>
+        <Footer/>
+        </div>
+    )
+}
