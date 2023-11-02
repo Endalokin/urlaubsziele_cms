69f7be841f81ab79aa19c59a3d5892614ae4cc9b
DESKTOP-AAHU0IQ\Tilo <info@tilorenner.com>
2023-11-02 15:00:34 +0100

    titleimg path corrected

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Cards from './Cards/Cards'
 import DetailPage from './DetailPage/DetailPage'
-import TitleImg from './Other/TitleIMG'
+import TitleImg from './Other/TitleImg'
 
 export default function Main() {
     return (
