6a4a6709d14147697e51f80cd6800381ff1a864d
Nora Westphal <tupfen@hotmail.de>
2023-11-09 09:33:58 +0100

    image-frame in dominant color on DetailPage

--- src/components/Main/DetailPage/DetailPage.jsx
+++ src/components/Main/DetailPage/DetailPage.jsx
@@ -50,6 +50,10 @@ export default function DetailPage({ }) {
     const shareurl = `https://cdn.contentful.com/spaces/${VITE_SPACE_ID}/assets/48CvpYGYtsrxDC9QQr2xi9?access_token=${VITE_CF_TOKEN}`
     const navurl = `https://cdn.contentful.com/spaces/${VITE_SPACE_ID}/assets/79MohGKY7i8ilc0OSAa288?access_token=${VITE_CF_TOKEN}`
 
+    const [imgBorderColor, setImgBorderColor] = useState("white")
+    const [containerBorderColor, setContainerBorderColor] = useState("50,50,50")
+
+
     useEffect(() => {
         fetchData(url, handleData);
         fetchData(shareurl, handleShareBtnURL);
@@ -67,8 +71,8 @@ export default function DetailPage({ }) {
             <div id="div_id" className="container py-4" >
                 <Head title={`Urlaubsziel ${_data.name}`} descr={`Wir präsentieren Ihnen: ${_data.tagline}`} />
                 <button onClick={() => navigate(-1)} className="btn mb-4">{`❮`}</button>
-                <div className="row bg-light bg-opacity-75 rounded">
-                    <div className="col">
+                <div className="row bg-light bg-opacity-75 rounded" >
+                    <div className="col p-4" style={{boxShadow: `0px 0px 40px 40px rgba(${containerBorderColor}, .1)`/* , borderBottom: `10px dotted rgba(${containerBorderColor}, .2)`, borderLeft: `10px dotted rgba(${containerBorderColor}, .2)`, borderBottom: `10px dotted rgba(${containerBorderColor}, .2)` */}}>
                         <div className="row">
                             <div id="selected_dest_name" className="col h3">
                                 {_data.name}
@@ -112,7 +116,7 @@ export default function DetailPage({ }) {
                                 </div>
                             </div>
                             <div className="col-12 col-md-6 text-center">
-                                <DetailPageImage assetID={_data.image.sys.id} classname={"img-fluid object-fit-cover rounded"} htmlID={"selected_dest_imagepath"} alt="country" style={{ height: "100%" }} />
+                                <DetailPageImage assetID={_data.image.sys.id} classname={"img-fluid object-fit-cover rounded"} setImgBorderColor={setImgBorderColor} setContainerBorderColor={setContainerBorderColor} containerBorderColor={containerBorderColor} imgBorderColor={imgBorderColor} htmlID={"selected_dest_imagepath"} alt="country" style={{ height: "100%", border: `10px solid ${imgBorderColor}` }} />
                             </div>
                         </div>
                         <div className="row">
